30932911ea836ce42858ccb364bc66458cd1927a
Ulyana <133405289+darkgooddack@users.noreply.github.com>
2025-02-14 14:37:35 +0300

    Update note.py

--- app/schemas/note.py
+++ app/schemas/note.py
@@ -2,27 +2,35 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+# id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String, nullable=False)
+#     description = Column(String, nullable=True)
+#     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+#     created_at = Column(DateTime, server_default=func.now())
+
+
 # Схема для создания заметки
 class NoteCreate(BaseModel):
     title: str
-    content: str
-    start_date: datetime
-    end_date: datetime
+    description: Optional[str] = None
+    user_id: int
+    created_date: datetime
+    deadline: Optional[datetime] = None
 
 # Схема для обновления заметки
 class NoteUpdate(BaseModel):
     title: Optional[str] = None
     content: Optional[str] = None
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
+    created_date: Optional[datetime] = None
+    deadline: Optional[datetime] = None
 
 # Схема для отображения заметки
 class Note(BaseModel):
     id: int
     title: str
     content: str
-    start_date: datetime
-    end_date: datetime
+    created_date: datetime
+    deadline: datetime
 
     class Config:
         from_attributes = True
